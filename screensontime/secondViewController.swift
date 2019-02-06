d51d60d1b808dc49ef7a482bcc97797bfcfe0f57
Hogoworld <hogoworld@hogoworlds-mac-mini.local>
2019-02-06 14:28:06 +0530

    1.added functionality in second screen 2.added segue from 1st screen to second screen

---
+++ screensontime/secondViewController.swift
@@ -0,0 +1,33 @@
+//
+//  secondViewController.swift
+//  screensontime
+//
+//  Created by Hogoworld on 06/02/19.
+//  Copyright © 2019 Hogoworld. All rights reserved.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    @IBAction func secondtoHome(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
